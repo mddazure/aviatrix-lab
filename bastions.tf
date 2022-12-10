cb7f5cd85199efede1e66e406bda5a52f63801ba
Marc de Droog <madedroo@microsoft.com>
2022-12-10 16:34:00 +0100

    first commit

--- bastions.tf
+++ bastions.tf
@@ -27,23 +27,23 @@ resource "azurerm_bastion_host" "bastion-spoke-1" {
 ## Create Bastion Hub
 #######################################################################
 resource "azurerm_public_ip" "bastion-hub-pubip" {
-  name                = "bastion-spoke-2-pubip"
-  location            = var.location-spoke-2
+  name                = "bastion-hub-1-pubip"
+  location            = var.location-hub
   resource_group_name = azurerm_resource_group.aviatrix-hub-rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_bastion_host" "bastion-spoke-2" {
-  name                = "bastion-spoke-2"
-  location            = var.location-spoke-2
+  name                = "bastion-hub-1"
+  location            = var.location-hub
   resource_group_name = azurerm_resource_group.aviatrix-hub-rg.name
   sku                 = "Standard"
   ip_connect_enabled =  true
   shareable_link_enabled = true
 
   ip_configuration {
-    name                 = "bastion-spoke-2-configuration"
+    name                 = "bastion-hub-1-configuration"
     subnet_id            = azurerm_subnet.bastion-hub-1-subnet.id
     public_ip_address_id = azurerm_public_ip.bastion-hub-1-pubip.id
   }
