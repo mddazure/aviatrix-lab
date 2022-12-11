3292fcf00367c12ec8ffabfc7bfd9d629870df59
Marc de Droog <madedroo@microsoft.com>
2022-12-11 16:36:01 +0100

    install iis

--- bastions.tf
+++ bastions.tf
@@ -24,6 +24,81 @@ resource "azurerm_bastion_host" "bastion-spoke-1" {
   }
 }
 #######################################################################
+## Create Bastion spoke-2
+#######################################################################
+resource "azurerm_public_ip" "bastion-spoke-2-pubip" {
+  name                = "bastion-spoke-2-pubip"
+  location            = var.location-spoke-2
+  resource_group_name = azurerm_resource_group.aviatrix-spoke-rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion-spoke-2" {
+  name                = "bastion-spoke-2"
+  location            = var.location-spoke-2
+  resource_group_name = azurerm_resource_group.aviatrix-spoke-rg.name
+  sku                 = "Standard"
+  ip_connect_enabled =  true
+  shareable_link_enabled = true
+
+  ip_configuration {
+    name                 = "bastion-spoke-2-configuration"
+    subnet_id            = azurerm_subnet.bastion-spoke-2-subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion-spoke-2-pubip.id
+  }
+}
+#######################################################################
+## Create Bastion spoke-3
+#######################################################################
+resource "azurerm_public_ip" "bastion-spoke-3-pubip" {
+  name                = "bastion-spoke-3-pubip"
+  location            = var.location-spoke-3
+  resource_group_name = azurerm_resource_group.aviatrix-spoke-rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion-spoke-3" {
+  name                = "bastion-spoke-3"
+  location            = var.location-spoke-3
+  resource_group_name = azurerm_resource_group.aviatrix-spoke-rg.name
+  sku                 = "Standard"
+  ip_connect_enabled =  true
+  shareable_link_enabled = true
+
+  ip_configuration {
+    name                 = "bastion-spoke-3-configuration"
+    subnet_id            = azurerm_subnet.bastion-spoke-3-subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion-spoke-3-pubip.id
+  }
+}
+#######################################################################
+## Create Bastion spoke-4
+#######################################################################
+resource "azurerm_public_ip" "bastion-spoke-4-pubip" {
+  name                = "bastion-spoke-4-pubip"
+  location            = var.location-spoke-4
+  resource_group_name = azurerm_resource_group.aviatrix-spoke-rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion-spoke-4" {
+  name                = "bastion-spoke-4"
+  location            = var.location-spoke-4
+  resource_group_name = azurerm_resource_group.aviatrix-spoke-rg.name
+  sku                 = "Standard"
+  ip_connect_enabled =  true
+  shareable_link_enabled = true
+
+  ip_configuration {
+    name                 = "bastion-spoke-4-configuration"
+    subnet_id            = azurerm_subnet.bastion-spoke-4-subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion-spoke-4-pubip.id
+  }
+}
+#######################################################################
 ## Create Bastion Hub
 #######################################################################
 resource "azurerm_public_ip" "bastion-hub-1-pubip" {
@@ -34,7 +109,7 @@ resource "azurerm_public_ip" "bastion-hub-1-pubip" {
   sku                 = "Standard"
 }
 
-resource "azurerm_bastion_host" "bastion-spoke-2" {
+resource "azurerm_bastion_host" "bastion-hub-1" {
   name                = "bastion-hub-1"
   location            = var.location-hub
   resource_group_name = azurerm_resource_group.aviatrix-hub-rg.name
