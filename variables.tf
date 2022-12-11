9a29032ae0b3103673699c024caf6592519d2b31
Marc de Droog <madedroo@microsoft.com>
2022-12-11 16:23:22 +0100

    install iis

--- variables.tf
+++ variables.tf
@@ -4,9 +4,26 @@ variable "location-hub" {
   //default     = "swedencentral"
 default = "westeurope"
 }
-
 variable "location-spoke-1" {
   description = "Location to deploy spoke-1"
+  type        = string
+  //default     = "swedencentral"
+default = "westeurope"
+}
+variable "location-spoke-2" {
+  description = "Location to deploy spoke-2"
+  type        = string
+  //default     = "swedencentral"
+default = "westeurope"
+}
+variable "location-spoke-3" {
+  description = "Location to deploy spoke-3"
+  type        = string
+  //default     = "swedencentral"
+default = "westeurope"
+}
+variable "location-spoke-4" {
+  description = "Location to deploy spoke-4"
   type        = string
   //default     = "swedencentral"
 default = "westeurope"
@@ -16,7 +33,6 @@ variable "username" {
   type        = string
   default     = "AzureAdmin"
 }
-
 variable "password" {
   description = "Virtual Machine password, must meet Azure complexity requirements"
    type        = string
@@ -26,37 +42,31 @@ variable "vmsize" {
   description = "Size of the VMs"
   default     = "Standard_D2s_v3"
 }
-
 variable "controller_name" {
     description = "name of Aviatrix controller"
     default = "Controller"
   
 }
-
 variable "controller_virtual_machine_admin_username" {
   type        = string
   description = "Admin Username for the controller virtual machine."
   default     = "aviatrix"
 }
-
 variable "controller_virtual_machine_admin_password" {
   type        = string
   description = "Admin Password for the controller virtual machine."
   default     = "aviatrix1234!"
 }
-
 variable "controller_virtual_machine_size" {
   type        = string
   description = "Virtual Machine size for the controller."
   default     = "Standard_A4_v2"
 }
-
 variable "incoming_ssl_cidr" {
   type        = list(string)
   default = [ "217.121.0.0/16" ]
   description = "Incoming cidr for security group used by controller"
 }
-
 variable "aad-sp-secret" {
   type = string
   default = "28V8Q~ti8VdGTf6dkgY.KZHZoOz.q4YRzU6TLdnY"
--- vnets.tf
+++ vnets.tf
@@ -78,6 +78,115 @@ resource "azurerm_subnet" "bastion-spoke-1-subnet" {
   address_prefixes       = ["10.1.2.0/24"]
 }
 #######################################################################
+## Create Virtual Network - Spoke 2
+#######################################################################
+resource "azurerm_virtual_network" "spoke-2-vnet" {
+  name                = "spoke-2-vnet"
+  location            = var.location-spoke-2
+  resource_group_name = azurerm_resource_group.aviatrix-spoke-rg.name
+  address_space       = ["10.2.0.0/16"]
+
+  tags = {
+    environment = "spoke-2"
+    deployment  = "terraform"
+    lab         = "aviatrix"
+  }
+}
+#######################################################################
+## Create Subnets - Spoke 2
+#######################################################################
+resource "azurerm_subnet" "spoke-2-aviatrix-subnet" {
+  name                 = "aviatrixSubnet"
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  virtual_network_name = azurerm_virtual_network.spoke-2-vnet.name
+  address_prefixes       = ["10.2.0.0/24"]
+}
+resource "azurerm_subnet" "spoke-2-vm-subnet" {
+  name                 = "vmSubnet"
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  virtual_network_name = azurerm_virtual_network.spoke-2-vnet.name
+  address_prefixes       = ["10.2.1.0/24"]
+}
+resource "azurerm_subnet" "bastion-spoke-2-subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  virtual_network_name = azurerm_virtual_network.spoke-2-vnet.name
+  address_prefixes       = ["10.2.2.0/24"]
+}
+#######################################################################
+## Create Virtual Network - Spoke 3
+#######################################################################
+resource "azurerm_virtual_network" "spoke-3-vnet" {
+  name                = "spoke-3-vnet"
+  location            = var.location-spoke-3
+  resource_group_name = azurerm_resource_group.aviatrix-spoke-rg.name
+  address_space       = ["10.3.0.0/16"]
+
+  tags = {
+    environment = "spoke-3"
+    deployment  = "terraform"
+    lab         = "aviatrix"
+  }
+}
+#######################################################################
+## Create Subnets - Spoke 3
+#######################################################################
+resource "azurerm_subnet" "spoke-3-aviatrix-subnet" {
+  name                 = "aviatrixSubnet"
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  virtual_network_name = azurerm_virtual_network.spoke-3-vnet.name
+  address_prefixes       = ["10.3.0.0/24"]
+}
+resource "azurerm_subnet" "spoke-3-vm-subnet" {
+  name                 = "vmSubnet"
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  virtual_network_name = azurerm_virtual_network.spoke-3-vnet.name
+  address_prefixes       = ["10.3.1.0/24"]
+}
+resource "azurerm_subnet" "bastion-spoke-3-subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  virtual_network_name = azurerm_virtual_network.spoke-3-vnet.name
+  address_prefixes       = ["10.3.2.0/24"]
+}
+#######################################################################
+## Create Virtual Network - Spoke 4
+#######################################################################
+resource "azurerm_virtual_network" "spoke-4-vnet" {
+  name                = "spoke-4-vnet"
+  location            = var.location-spoke-4
+  resource_group_name = azurerm_resource_group.aviatrix-spoke-rg.name
+  address_space       = ["10.4.0.0/16"]
+
+  tags = {
+    environment = "spoke-4"
+    deployment  = "terraform"
+    lab         = "aviatrix"
+  }
+}
+#######################################################################
+## Create Subnets - Spoke 1
+#######################################################################
+resource "azurerm_subnet" "spoke-4-aviatrix-subnet" {
+  name                 = "aviatrixSubnet"
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  virtual_network_name = azurerm_virtual_network.spoke-4-vnet.name
+  address_prefixes       = ["10.4.0.0/24"]
+}
+resource "azurerm_subnet" "spoke-1-vm-subnet" {
+  name                 = "vmSubnet"
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  virtual_network_name = azurerm_virtual_network.spoke-4-vnet.name
+  address_prefixes       = ["10.4.1.0/24"]
+}
+resource "azurerm_subnet" "bastion-spoke-1-subnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  virtual_network_name = azurerm_virtual_network.spoke-4-vnet.name
+  address_prefixes       = ["10.4.2.0/24"]
+}
+
+#######################################################################
 ## Create Network Interface - Hub 1
 #######################################################################
 resource "azurerm_network_interface" "hub-1-nic" {
@@ -120,6 +229,69 @@ resource "azurerm_network_interface" "spoke-1-nic" {
   }
 }
 #######################################################################
+## Create Network Interface - Spoke 2
+#######################################################################
+resource "azurerm_network_interface" "spoke-2-nic" {
+  name                 = "spoke-12-nic"
+  location             = var.location-spoke-2
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  enable_ip_forwarding = false
+
+  ip_configuration {
+    name                          = "spoke-2-ipconfig"
+    subnet_id                     = azurerm_subnet.spoke-2-vm-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    environment = "spoke-2"
+    deployment  = "terraform"
+    lab         = "aviatrix"
+  }
+}
+#######################################################################
+## Create Network Interface - Spoke 3
+#######################################################################
+resource "azurerm_network_interface" "spoke-3-nic" {
+  name                 = "spoke-3-nic"
+  location             = var.location-spoke-3
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  enable_ip_forwarding = false
+
+  ip_configuration {
+    name                          = "spoke-3-ipconfig"
+    subnet_id                     = azurerm_subnet.spoke-3-vm-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    environment = "spoke-3"
+    deployment  = "terraform"
+    lab         = "aviatrix"
+  }
+}
+#######################################################################
+## Create Network Interface - Spoke 4
+#######################################################################
+resource "azurerm_network_interface" "spoke-4-nic" {
+  name                 = "spoke-4-nic"
+  location             = var.location-spoke-4
+  resource_group_name  = azurerm_resource_group.aviatrix-spoke-rg.name
+  enable_ip_forwarding = false
+
+  ip_configuration {
+    name                          = "spoke-4-ipconfig"
+    subnet_id                     = azurerm_subnet.spoke-1-vm-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+  tags = {
+    environment = "spoke-4"
+    deployment  = "terraform"
+    lab         = "aviatrix"
+  }
+}
+#######################################################################
 ## Create Virtual Machine hub-1
 #######################################################################
 resource "azurerm_windows_virtual_machine" "hub-1-vm" {
@@ -152,8 +324,20 @@ resource "azurerm_windows_virtual_machine" "hub-1-vm" {
     lab         = "aviatrix"
   }
 }
+resource "azurerm_virtual_machine_extension" "install-iis-hub-1-vm" {
+    
+  name                 = "install-iis-hub-1-vm"
+  virtual_machine_id   = azurerm_windows_virtual_machine.hub-1-vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
 
-
+   settings = <<SETTINGS
+    {
+        "commandToExecute":"powershell -ExecutionPolicy Unrestricted Add-WindowsFeature Web-Server; powershell -ExecutionPolicy Unrestricted Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"
+    }
+SETTINGS
+}
 #######################################################################
 ## Create Virtual Machine spoke-1
 #######################################################################
@@ -186,4 +370,159 @@ resource "azurerm_windows_virtual_machine" "spoke-1-vm" {
     deployment  = "terraform"
     lab         = "aviatrix"
   }
+}
+resource "azurerm_virtual_machine_extension" "install-iis-spoke-1-vm" {
+    
+  name                 = "install-iis-spoke-1-vm"
+  virtual_machine_id   = azurerm_windows_virtual_machine.spoke-1-vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+   settings = <<SETTINGS
+    {
+        "commandToExecute":"powershell -ExecutionPolicy Unrestricted Add-WindowsFeature Web-Server; powershell -ExecutionPolicy Unrestricted Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"
+    }
+SETTINGS
+}
+#######################################################################
+## Create Virtual Machine spoke-2
+#######################################################################
+resource "azurerm_windows_virtual_machine" "spoke-2-vm" {
+  name                  = "spoke-2-vm"
+  location              = var.location-spoke-2
+  resource_group_name   = azurerm_resource_group.aviatrix-spoke-rg.name
+  network_interface_ids = [azurerm_network_interface.spoke-2-nic.id]
+  size               = var.vmsize
+  computer_name  = "spoke-2-vm"
+  admin_username = var.username
+  admin_password = var.password
+  provision_vm_agent = true
+
+  source_image_reference {
+    offer     = "WindowsServer"
+    publisher = "MicrosoftWindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+
+  os_disk {
+    name              = "spoke-2-osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+  
+  tags = {
+    environment = "spoke-2"
+    deployment  = "terraform"
+    lab         = "aviatrix"
+  }
+}
+resource "azurerm_virtual_machine_extension" "install-iis-spoke-2-vm" {
+    
+  name                 = "install-iis-spoke-2-vm"
+  virtual_machine_id   = azurerm_windows_virtual_machine.spoke-1-vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+   settings = <<SETTINGS
+    {
+        "commandToExecute":"powershell -ExecutionPolicy Unrestricted Add-WindowsFeature Web-Server; powershell -ExecutionPolicy Unrestricted Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"
+    }
+SETTINGS
+}
+#######################################################################
+## Create Virtual Machine spoke-3
+#######################################################################
+resource "azurerm_windows_virtual_machine" "spoke-3-vm" {
+  name                  = "spoke-3-vm"
+  location              = var.location-spoke-3
+  resource_group_name   = azurerm_resource_group.aviatrix-spoke-rg.name
+  network_interface_ids = [azurerm_network_interface.spoke-3-nic.id]
+  size               = var.vmsize
+  computer_name  = "spoke-3-vm"
+  admin_username = var.username
+  admin_password = var.password
+  provision_vm_agent = true
+
+  source_image_reference {
+    offer     = "WindowsServer"
+    publisher = "MicrosoftWindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+
+  os_disk {
+    name              = "spoke-3-osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+  
+  tags = {
+    environment = "spoke-3"
+    deployment  = "terraform"
+    lab         = "aviatrix"
+  }
+}
+resource "azurerm_virtual_machine_extension" "install-iis-spoke-3-vm" {
+    
+  name                 = "install-iis-spoke-3-vm"
+  virtual_machine_id   = azurerm_windows_virtual_machine.spoke-1-vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+   settings = <<SETTINGS
+    {
+        "commandToExecute":"powershell -ExecutionPolicy Unrestricted Add-WindowsFeature Web-Server; powershell -ExecutionPolicy Unrestricted Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"
+    }
+SETTINGS
+}
+#######################################################################
+## Create Virtual Machine spoke-4
+#######################################################################
+resource "azurerm_windows_virtual_machine" "spoke-4-vm" {
+  name                  = "spoke-4-vm"
+  location              = var.location-spoke-4
+  resource_group_name   = azurerm_resource_group.aviatrix-spoke-rg.name
+  network_interface_ids = [azurerm_network_interface.spoke-4-nic.id]
+  size               = var.vmsize
+  computer_name  = "spoke-4-vm"
+  admin_username = var.username
+  admin_password = var.password
+  provision_vm_agent = true
+
+  source_image_reference {
+    offer     = "WindowsServer"
+    publisher = "MicrosoftWindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+
+  os_disk {
+    name              = "spoke-4-osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+  
+  tags = {
+    environment = "spoke-4"
+    deployment  = "terraform"
+    lab         = "aviatrix"
+  }
+}
+resource "azurerm_virtual_machine_extension" "install-iis-spoke-4-vm" {
+    
+  name                 = "install-iis-spoke-4-vm"
+  virtual_machine_id   = azurerm_windows_virtual_machine.spoke-1-vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+   settings = <<SETTINGS
+    {
+        "commandToExecute":"powershell -ExecutionPolicy Unrestricted Add-WindowsFeature Web-Server; powershell -ExecutionPolicy Unrestricted Add-Content -Path \"C:\\inetpub\\wwwroot\\Default.htm\" -Value $($env:computername)"
+    }
+SETTINGS
 }
