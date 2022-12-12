1cb28f70f1a4ded8f9a11bdd327ce7142891929d
Marc de Droog <madedroo@microsoft.com>
2022-12-12 13:17:18 +0100

    aviatrix-transit-spoke

--- aviatrix-deploy.tf
+++ aviatrix-deploy.tf
@@ -11,6 +11,9 @@ provider "aviatrix" {
   controller_ip = azurerm_public_ip.aviatrix_controller_public_ip.ip_address
   username = var.controller_admin_username
   password = var.controller_admin_password  
+  depends_on = [
+    azurerm_public_ip.aviatrix_controller_public_ip
+  ]
 }
 
 #######################################################################
