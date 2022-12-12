70f83b2606163a5ce653ca5cafcf70d4eaee4d40
Marc de Droog <madedroo@microsoft.com>
2022-12-12 12:28:25 +0100

    aviatrix-transit-spoke

--- aviatrix-deploy.tf
+++ aviatrix-deploy.tf
@@ -99,7 +99,7 @@ resource "aviatrix_transit_gateway" "transit_gw" {
   account_name      = "MdD_1_non_prod"
   vpc_reg           = var.location-hub
   vpc_id            = azurerm_virtual_network.hub-1-vnet.id
-  subnet            = azurerm_subnet.hub-1-aviatrix-gateway-subnet.address_prefixes
+  subnet            = azurerm_subnet.hub-1-aviatrix-gateway-subnet.address_prefixes[0]
   gw_size           = "Standard_B1ms"
   gw_name           = "transit-gw-hub-1"
   allocate_new_eip  = true
@@ -111,7 +111,7 @@ resource "aviatrix_spoke_gateway" "spoke_1_gw" {
   account_name      = "MdD_1_non_prod"
   vpc_reg           = var.location-spoke-1
   vpc_id            = azurerm_virtual_network.spoke-1-vnet.id
-  subnet            = azurerm_subnet.spoke-1-aviatrix-subnet.address_prefixes
+  subnet            = azurerm_subnet.spoke-1-aviatrix-subnet.address_prefixes[0]
   gw_size           = "Standard_B1ms"
   gw_name           = "spoke-gw-1"
   allocate_new_eip  = true
