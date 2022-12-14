774aebd5ff0190f5f1e8ed8d585d3501205109aa
Marc de Droog <madedroo@microsoft.com>
2022-12-14 08:38:30 +0100

    remove nsgs

---
+++ nsg.sh
@@ -0,0 +1,18 @@
+
+az network vnet subnet update -g aviatrix-hub-rg --vnet-name hub-1-vnet --name aviatrixControllerSubnet --network-security-group ""
+az network vnet subnet update -g aviatrix-hub-rg --vnet-name hub-1-vnet --name aviatrixGWSubnet --network-security-group ""
+
+az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-1-vnet --name aviatrixSubnet --network-security-group ""
+az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-1-vnet --name vmSubnet --network-security-group ""
+
+az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-2-vnet --name aviatrixSubnet --network-security-group ""
+az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-2-vnet --name vmSubnet --network-security-group ""
+
+az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-3-vnet --name aviatrixSubnet --network-security-group ""
+az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-3-vnet --name vmSubnet --network-security-group ""
+
+az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-4-vnet --name aviatrixSubnet --network-security-group ""
+az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-4-vnet --name vmSubnet --network-security-group ""
+
+
+
