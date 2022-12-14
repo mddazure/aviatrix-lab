f4730085e0ff0a41dc026f407b99e494d8ff28c9
Marc de Droog <madedroo@microsoft.com>
2022-12-14 14:32:00 +0100

    remove nsgs

--- nsg.sh
+++ nsg.sh
@@ -11,6 +11,7 @@ do
     az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-3-vnet --name vmSubnet --network-security-group ""
     az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-4-vnet --name aviatrixSubnet --network-security-group ""
     az network vnet subnet update -g aviatrix-spoke-rg --vnet-name spoke-4-vnet --name vmSubnet --network-security-group ""
+    echo "waiting 60 seconds"
     sleep 60
 done
 
