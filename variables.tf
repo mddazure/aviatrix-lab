66d2c2f4dc4884cd83caaecd323f7f08319f2f25
Marc de Droog <madedroo@microsoft.com>
2022-12-14 17:47:25 +0100

    move contr

--- variables.tf
+++ variables.tf
@@ -50,7 +50,7 @@ variable "vmsize" {
   description = "Size of the VMs"
   default     = "Standard_D2s_v3"
 }
-variable "controller_ip" {
+variable "controller-ip" {
   description   = "public ip of controller"
   default       = "52.178.40.245"
 }
